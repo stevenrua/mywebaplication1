e2156b31a972593b229cc25bb3c5297a8af50826
steven <srua10@misena.edu.co>
2020-10-27 15:07:07 -0500

    version 1

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService} from '../service/auth.service';
-import { Router } from '@angular/router'
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +19,8 @@ export class LoginComponent implements OnInit {
   }
 
   constructor(private auth:AuthService,
-              private router:Router) { }
+              private router:Router,
+              private snackBar : MatSnackBar) { }
 
   ngOnInit(): void {
   }
@@ -30,7 +33,17 @@ export class LoginComponent implements OnInit {
           localStorage.setItem('token',res.jwtoken)
           this.router.navigate(['/tasks'])
         },
-        err=>console.log(err)
+        err=>{
+          console.log(err)
+          if(err instanceof HttpErrorResponse){
+            if(err.status === 400){
+                this.snackBar.open("Credenciales Incorrectas",null,{
+                  duration : 2000
+                })
+                this.router.navigate(['/login'])
+            }
+          }
+        }
       )
   }
 
